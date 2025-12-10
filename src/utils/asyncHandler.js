7a99cd465faabc94653a36d019f8a52a2a108c56
rajankumar-dev <rk1620703@gmail.com>
2025-12-10 19:39:47 +0530

    complete first api setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHadler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHadler(req, res, next)).
         catch((err) => next(err))
     }
